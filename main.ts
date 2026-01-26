297ef97d5b7bbf43d18bbfc7d0594e206eded686
unknown <devcoda25@gmail.com>
2026-01-26 16:14:58 +0300

    find

--- main.ts
+++ main.ts
@@ -2,4 +2,5 @@ import 'dotenv/config';
 import 'reflect-metadata';
 import { bootstrap } from './server/server';
 
+console.log('>>> SYSTEM BOOT: VERSION V4.1 (COOKIE_RENAME_STABLE) <<<');
 bootstrap();
