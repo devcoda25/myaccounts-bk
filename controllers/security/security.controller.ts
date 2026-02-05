9f04d8532055d162460038a15ce84f9e891a9af1
unknown <devcoda25@gmail.com>
2026-02-05 15:10:54 +0300

    find

--- controllers/security/security.controller.ts
+++ controllers/security/security.controller.ts
@@ -9,6 +9,36 @@ import { AuthRequest } from '../../common/interfaces/auth-request.interface';
 export class SecurityController {
     constructor(private service: SecurityService) { }
 
+    @Get('overview')
+    async getOverview(@CurrentUser() user: AuthRequest['user']) {
+        // Return a comprehensive security overview for the frontend
+        return {
+            password: {
+                lastChangedDays: 0, // TODO: Get from user service
+                strength: 0, // TODO: Calculate from password rules
+                compromised: false
+            },
+            mfa: {
+                enabled: false, // TODO: Get from MFA service
+                methods: [],
+                recoveryCodesRemaining: 0
+            },
+            passkeys: {
+                enabled: false, // TODO: Get from passkeys service
+                count: 0
+            },
+            recovery: {
+                verifiedEmails: 1,
+                verifiedPhones: 0
+            }
+        };
+    }
+
+    @Get('activity')
+    async getActivities(@CurrentUser() user: AuthRequest['user']) {
+        return this.service.getActivityLogs(user.id);
+    }
+
     @Post('reports')
     async report(@CurrentUser() user: AuthRequest['user'], @Body() body: { type: string, reason: string, details: string }, @Ip() ip: string) {
         return this.service.reportIncident(user.id, { ...body, ip });
@@ -17,10 +47,5 @@ export class SecurityController {
     @Post('lock')
     async lock(@CurrentUser() user: AuthRequest['user'], @Ip() ip: string) {
         return this.service.lockAccount(user.id, ip);
-    }
-
-    @Get('activity')
-    async getActivities(@CurrentUser() user: AuthRequest['user']) {
-        return this.service.getActivityLogs(user.id);
     }
 }
