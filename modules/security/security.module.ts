7cb876b2726a28393da2f98945da6d0eee2e81c5
unknown <devcoda25@gmail.com>
2026-02-06 15:45:10 +0300

    find

--- modules/security/security.module.ts
+++ modules/security/security.module.ts
@@ -1,12 +1,29 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../../prisma-lib/prisma.module';
+import { AuthModule } from '../auth/auth.module';
+import { UsersModule } from '../users/users.module';
 import { SecurityService } from '../../services/security/security.service';
+import { MfaService } from '../../services/auth/mfa.service';
+import { PasskeysService } from '../../services/auth/passkeys.service';
+import { PasswordService } from '../../services/auth/password.service';
+import { SessionRepository } from '../../repos/auth/session.repository';
+import { SessionManagementService } from '../../services/auth/session-management.service';
 import { SecurityController } from '../../controllers/security/security.controller';
-import { AuthModule } from '../auth/auth.module'; // For AuthGuard dependencies if needed, usually AuthModule exports JwtModule etc.
 
 @Module({
-    imports: [PrismaModule],
-    providers: [SecurityService],
+    imports: [
+        PrismaModule,
+        AuthModule,
+        UsersModule
+    ],
+    providers: [
+        SecurityService,
+        MfaService,
+        PasskeysService,
+        PasswordService,
+        SessionRepository,
+        SessionManagementService
+    ],
     controllers: [SecurityController],
     exports: [SecurityService]
 })
